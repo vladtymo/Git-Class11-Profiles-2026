3a59a424bee34040c3109ad9c020e013f98b4bfb
Maksym <theplytka@gmail.com>
2026-09-21 16:03:17 +0300

    max made a profile

---
+++ profiles/Maksym/h.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Inter', 'sans-serif'],
+            },
+            colors: {
+                darkbg: '#0B1120',
+                cardbg: '#151E2E',
+            }
+        }
+    }
+}
